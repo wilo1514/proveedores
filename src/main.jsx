2a756ab479f67ba4b10b3287f5c66012b045e50f
wilo1514 <williamlituma072@gmail.com>
2025-04-22 09:26:40 -0500

    vite confing 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
-
